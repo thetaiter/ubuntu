f75b56efbeffb5c0fe9fbbae5efcbf380a7c94c5
Ian Tait <thetaiter@gmail.com>
2015-04-06 22:40:27 -0500

    Rearranged commands to fix glitch

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -4,18 +4,11 @@ cwd=${PWD}
 
 path=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
-printf '\nRemoving unnecessary directories...\n'
-sudo rm -r -f ~/Music ~/Public ~/Videos ~/examples.desktop ~/Pictures ~/Templates ~/Downloads/ubuntu-master*
-
-printf '\nAdding Development directory...\n'
-mkdir -p ~/Development
-
-printf '\nChanging to directory %s...\n' $path
-cd $path
-
 printf '\nReplaceing ~/.bashrc...\n'
+cd $path
 sudo rm ~/.bashrc
-sudo cp $path/.bashrc ~/.bashrc
+sudo cp .bashrc ~/.bashrc
+cd $cwd
 
 printf '\nInstalling programs...\n'
 sudo apt-get -y install byobu htop vim git build-essential > /dev/null
@@ -29,8 +22,11 @@ git config --global user.name "Ian Tait"
 git config --global user.email "thetaiter@gmail.com"
 git config --global push.default simple
 
-printf '\nReturning to original directory...\n'
-cd $cwd
+printf '\nRemoving unnecessary directories...\n'
+sudo rm -r -f ~/Music ~/Public ~/Videos ~/examples.desktop ~/Pictures ~/Templates ~/Downloads/ubuntu-master*
+
+printf '\nAdding Development directory...\n'
+mkdir -p ~/Development
 
 printf '\nDone! Rebooting now...\n'
 sudo shutdown -r now
