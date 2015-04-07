7c192cc5b01a7ac5ad5d09b24458608c11103175
Ian Tait <thetaiter@gmail.com>
2015-04-06 19:17:15 -0500

    Added .gitignore and comments

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -4,22 +4,26 @@ cwd=${PWD}
 
 path=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
-printf 'Changing to directory %s' $path
+printf '\nChanging to directory %s...\n\n' $path
 cd $path
 
+printf '\nReplaceing ~/.bashrc...\n\n'
 sudo rm ~/.bashrc
 sudo cp .bashrc ~/.bashrc
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-
+printf '\nInstalling programs...\n\n'
 sudo apt-get -y install byobu htop vim git build-essential
 
+printf '\nRunning Updates and Upgrades...\n\n'
 sudo apt-get -y update
 sudo apt-get -y upgrade
 
+printf '\nConfiguring git...\n\n'
 git config --global user.name "Ian Tait"
 git config --global user.email "thetaiter@gmail.com"
 git config --global push.default simple
 
+printf '\nReturning to original directory...\n\n'
 cd $cwd
+
+printf '\nDone!\n\n'
