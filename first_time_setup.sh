54cc9aec53ee44f76e9e446c8790890e25fe7284
Ian Tait <thetaiter@gmail.com>
2015-04-06 19:51:07 -0500

    Fixed spacing and made apt-get quiet

--- first_time_setup.sh
+++ first_time_setup.sh
@@ -4,26 +4,26 @@ cwd=${PWD}
 
 path=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 
-printf '\nChanging to directory %s...\n\n' $path
+printf '\nChanging to directory %s...\n' $path
 cd $path
 
-printf '\nReplaceing ~/.bashrc...\n\n'
+printf '\nReplaceing ~/.bashrc...\n'
 sudo rm ~/.bashrc
 sudo cp .bashrc ~/.bashrc
 
-printf '\nInstalling programs...\n\n'
-sudo apt-get -y install byobu htop vim git build-essential
+printf '\nInstalling programs...\n'
+sudo apt-get -y install byobu htop vim git build-essential > /dev/null
 
-printf '\nRunning Updates and Upgrades...\n\n'
-sudo apt-get -y update
-sudo apt-get -y upgrade
+printf '\nRunning Updates and Upgrades...\n'
+sudo apt-get -y update > /dev/null
+sudo apt-get -y upgrade > /dev/null
 
-printf '\nConfiguring git...\n\n'
+printf '\nConfiguring git...\n'
 git config --global user.name "Ian Tait"
 git config --global user.email "thetaiter@gmail.com"
 git config --global push.default simple
 
-printf '\nReturning to original directory...\n\n'
+printf '\nReturning to original directory...\n'
 cd $cwd
 
-printf '\nDone!\n\n'
+printf '\nDone!\n'
